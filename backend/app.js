1a53b98c94dcf561469fea5bcca8bf8606d8a015
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-24 00:19:09 -0500

    shoudlve set up the backend earlier

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,11 @@ const express = require('express');
 const app = express(); 
 
 
-app.use(express.json()); // middleware to handle requests. 
+app.use(express.json()); // middleware to handle requests.   
+
+app.get('/', (req, res) => { 
+    res.json({ 
+        Server: 'Running Successfully'
+    }); 
+}); 
+
