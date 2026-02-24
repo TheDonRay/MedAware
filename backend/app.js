b75780c07a2454194d6fc080a3331dfdfeafd808
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-24 00:20:33 -0500

    working ont he backend

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express(); 
 
 
-app.use(express.json()); // middleware to handle requests.   
+app.use(express.json()); // middleware to handle requests.    
+
+const PORT = process.env.PORT; 
 
 app.get('/', (req, res) => { 
     res.json({ 
         Server: 'Running Successfully'
     }); 
+});  
+
+app.listen(PORT, () => { 
+    console.log(`Server is up and running on http://localhost:PORT`); 
 }); 
+
+module.exports = app; 
 
