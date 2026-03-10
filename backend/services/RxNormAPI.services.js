f23ba0d478b8dd003faab85df69258cfecf77843
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-03-09 20:44:27 -0400

    working on the backend

--- backend/services/RxNormAPI.services.js
+++ backend/services/RxNormAPI.services.js
@@ -1,12 +1,13 @@
 
 // external service here which sends data to rxNorm API.  Remember that we are calling this external function from another controller.  
 const externalAPI = async (medicineAdvised) => {  
+    const endpoints = new URLSearchParams({ 
+        name: medicineAdvised
+    });  
+    // made the URL here as such 
+    const URL = `https://rxnav.nlm.nih.gov/REST/drugs.json?${endpoints}`; 
     
-    // define the function here    
-    const exampledatatoreturn = { 
-        data: medicineAdvised
-    }; 
-    return exampledatatoreturn;  
+    return URL;  
 } 
 
 module.exports = { externalAPI }; 
